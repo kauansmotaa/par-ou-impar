6206e55806a0fae288bc7423d62feeea509c96f9
kauansmotaa <kauansmota.xbox@gmail.com>
2025-03-23 22:13:32 -0300

    j

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 function verificarParOuImpar() {
     let numero = document.getElementById('numero').value;
     let resultado = document.getElementById('resultado');
-
-    if (numero === '') {
-        resultado.textContent = 'Por favor, digite um número!';
-        resultado.style.color = 'red';
+    
+    if (numero === "") {
+        resultado.textContent = "Por favor, insira um número válido!";
         return;
     }
-
+    
     numero = parseInt(numero);
-
-    if (numero % 2 === 0) {
-        resultado.textContent = `O número ${numero} é PAR.`;
-        resultado.style.color = 'blue';
+    
+    if (isNaN(numero)) {
+        resultado.textContent = "Isso não é um número válido!";
+    } else if (numero % 2 === 0) {
+        resultado.textContent = `O número ${numero} é Par!`;
     } else {
-        resultado.textContent = `O número ${numero} é ÍMPAR.`;
-        resultado.style.color = 'green';
+        resultado.textContent = `O número ${numero} é Ímpar!`;
     }
 }
